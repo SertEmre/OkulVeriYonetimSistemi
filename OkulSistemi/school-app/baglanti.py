1325bd09fc8e850769838ccc89ed8f8841e0c5b4
SertEmre <emresert2233@gmail.com>
2025-05-22 01:01:22 +0300

    Update baglanti.py

--- OkulSistemi/school-app/baglanti.py
+++ OkulSistemi/school-app/baglanti.py
@@ -35,10 +35,26 @@ class DbManager:
             print("hata:",err)
 
     def öğrencikayıt(self,student: Student):  
-        pass
+        sql = "INSERT INTO öğrenciler(okulnumarası,isim,soyisim,doğumtarihi,cinsiyet,sınıfId) VALUES (%s,%s,%s,%s,%s,%s)"
+        value =(student.okulnumarası,student.isim,student.soyisim,student.doğumtarihi,student.cinsiyet,student.sınıfId)
+        self.cursor.execute(sql,value)
+
+        try:
+            self.connection.commit()
+            print(f"{self.cursor.rowcount} tane kayıt eklendi.")
+        except mysql.connector.Error as err:
+            print('Hata:',err)
     
     def öğrencidüzenle(self,student:Student):
-        pass
+        sql = "UPDATE öğrenciler SET isim=%s, soyisim=%s, doğumtarihi=%s, cinsiyet=%s, sınıfId=%s WHERE okulnumarası=%s" 
+        value = (student.isim, student.soyisim, student.doğumtarihi, student.cinsiyet, student.sınıfId, student.okulnumarası)
+        self.cursor.execute(sql,value)
+
+        try:
+            self.connection.commit()
+            print(f"{self.cursor.rowcount} tane kayıt güncellendi.")
+        except mysql.connector.Error as err:
+            print('Hata:',err)
 
     def öğretmendüzenle(self,teacher:Teacher):
         pass
@@ -46,11 +62,30 @@ class DbManager:
     def öğretmenkayıt(self,teacher:Teacher):
         pass       
 
+    def __del__(self):
+        self.connection.close()
+        print("db silindi")
+
 db = DbManager(connection)
 
-students = db.öğrencigetirId(1)
-if students:
-    student = students[0]
-    print(f"Öğrenci: {student.isim} {student.soyisim}")
-else:
-    print("Bu okul numarasına ait öğrenci bulunamadı!")
+#-----------------------------------Öğrenci Düzenle
+# student =db.öğrencigetirId(1010)
+# if isinstance(student,list):
+#     student = student[0]
+#     student.isim = "Fırat"
+#     db.öğrencidüzenle(student)
+# else:
+#     print("Değiştirilmek istenen Öğrenci bulunamadı")
+
+#-----------------------------------Öğrenci kayıt
+# doğumtarihi = datetime(2005,2,17)
+# std = Student(1011,"Emre","Sert",doğumtarihi,"E",1)
+
+# db.öğrencikayıt(std)
+#-----------------------------------------------Öğrenci gösterme
+#students = db.öğrencigetirId(1001)
+# if students:
+#     student = students[0]
+#     print(f"Öğrenci: {student.isim} {student.soyisim}")
+# else:
+#     print("Bu okul numarasına ait öğrenci bulunamadı!")
